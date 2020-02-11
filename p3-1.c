5b24b3fca6f03aca8b1d628b312b19227007f582
edz <edz@chuxq.local>
2020-02-11 10:17:54 +0800

    p3-1.c

---
+++ p3-1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(void){
+    int a, b;
+    puts("请输入两个整数");
+    printf("整数A:");
+    scanf("%d", &a);
+    printf("整数B:");
+    scanf("%d", &b);
+    if(a % b == 0){
+        printf("B是A的约数\n");
+    }
+    else{
+        printf("B不是A的约数\n");
+    }
+    return 0;
+
+}
