f884ee9fc223d2eca723ca83d6650a71d6879990
edz <edz@chuxq.local>
2020-02-11 10:48:58 +0800

    p3-4.c

---
+++ p3-4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(void){
+    int a, b;
+    puts("输入两个整数");
+    printf("整数A:");
+    scanf("%d", &a);
+    printf("整数B:");
+    scanf("%d", &b);
+    if(a == b){
+        printf("A和B相等\n");
+    }
+    else if(a > b){
+        printf("A大于B\n");
+    }
+    else{
+        printf("A小于B\n");
+    }
+    return 0;
+}
