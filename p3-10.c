5a658eceaf5ea05d72238431b0fdee12ee05af9b
edz <edz@chuxq.local>
2020-02-11 17:44:52 +0800

    p3-10.c

---
+++ p3-10.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(void){
+    int a, b, c;
+    printf("整数1:");
+    scanf("%d", &a);
+    printf("整数2:");
+    scanf("%d", &b);
+    printf("整数3:");
+    scanf("%d", &c);
+    if(a == b){
+        if(a == c){
+            printf("三个值都相等\n");
+        }
+        else{
+            printf("有两个值相等\n");
+        }
+    }
+    else{
+        if(a == c){
+            printf("有两个值相等\n");
+        }
+        else if(b == c){
+            printf("有两个值相等\n");
+        }
+        else{
+            printf("三个值各不相同\n");
+        }
+    }
+    return 0;
+}
